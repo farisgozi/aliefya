7c702afa6e4a842629948ef376a7664293aaea5f
farisgozi <fariscraft709@gmail.com>
2026-05-30 14:54:31 +0700

    feat: configure image domains in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "assets.aceternity.com",
+      "images.tokopedia.net",
+    ],
+  },
 };
 
 export default nextConfig;
